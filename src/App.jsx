20866bf4609227b6be6251d7ff3241e02735c65d
Nebi Koray Kaya <onyekoray@gmail.com>
2025-07-04 20:21:44 +0300

    component eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Card from './Components/Card'
+import Header from './Components/Header'
 
 export default function App() {
 
   return (
     <>
       test
+      <Header />
+      <Card />
     </>
   )
 }
