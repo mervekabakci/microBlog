e50b19df2f1235ee6febdc5cad8b63d534131fee
TriosMerve <merve@trios.com.tr>
2025-07-05 10:36:14 +0300

    component yapılarında duzenlemeler, sass componentlerınde duzenlemeler, header ve footer da duzenlemeler yapıldı. font ıcon set eklendi. Mobil header duzenlendi.

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <footer>
+            <div className="container-md footer">
+                <span className="copyRight">Copyright © 2025 MicroBlog </span>
+            </div>
+        </footer>
+    )
+}
